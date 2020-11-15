5dee4459b79e01a64151e8d9f89f45457c7e1553
Lucius VanSlyke <lu.vanslyke@gmail.com>
2020-11-15 04:30:06 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -23,6 +23,7 @@ def consolidate_cart(cart)
   while counter < cart.length 
     new_item = find_item_by_name_in_collection(cart[counter][:item], new_cart)
     if new_item != nil
+      new_item
     counter = counter + 1
   
 
